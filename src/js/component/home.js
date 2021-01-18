3490cd7ceb81708e01c6fa37f061ed05b1cb4c3c
Jose Hurtado <hello@josehurtado.net>
2021-01-18 17:55:41 +0000

    NavBar edits

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,49 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
+		// NavBar
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<a className="navbar-brand" href="#">
+				Navbar
 			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon" />
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNav">
+				<ul className="navbar-nav ml-auto">
+					<li className="nav-item active">
+						<a className="nav-link" href="#">
+							Home <span className="sr-only">(current)</span>
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Features
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Pricing
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link disabled" href="#">
+							Disabled
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+		// Jumbotron
 	);
 }
